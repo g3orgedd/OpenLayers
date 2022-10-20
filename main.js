138c6cfbf90970451ddb863351f2347141177b7d
g3orgedd <g3orgedd@gmail.com>
2022-10-20 14:56:57 +0300

    Added SKFO data to project

--- main.js
+++ main.js
@@ -73,14 +73,14 @@ function init() {
     }
 
     // SKFO Layer - Северо-Кавказский федеральный округ
-    const SKFOGeoJSON = new ol.layer.VectorImage({
+    const NorthOsetiaGeoJSON = new ol.layer.VectorImage({
         source: new ol.source.Vector({
-            url: './data/NorthOsetiaRepublic.geojson',
+            url: './data/SKFO/North_Osetia_Republic.geojson',
             format: new ol.format.GeoJSON()
         }),
         visible: true,
         title: 'SKFO'
     })
 
-    map.addLayer(SKFOGeoJSON);
+    map.addLayer(NorthOsetiaGeoJSON);
 }
