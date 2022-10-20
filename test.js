d36be44211ee707eb73884b44c23fe406c2b91b1
g3orgedd <g3orgedd@gmail.com>
2022-10-20 23:53:55 +0300

    Added function to add markers

---
+++ test.js
@@ -0,0 +1,111 @@
+window.onload = init;
+
+function init() {
+    const map = new ol.Map({
+        view: new ol.View({
+            center: [0, 0], // центрирование карты
+            // maxZoom: 15, // максимальный зум
+            minZoom: 2, // минимальный зум
+            zoom: 2
+        }),
+        target: 'map'
+    })
+    
+    // Open Street Map
+    const OSM = new ol.layer.Tile({
+        title: 'OSMStandart',
+        source: new ol.source.OSM(),
+        visible: false
+        // visible: true
+    })
+
+    // Google Map
+
+    /*
+        Тип карты:
+            lyrs=h - roads only (только дороги)
+            lyrs=m - standard roadmap (стандартная карта дорог)
+            lyrs=p - terrain (ландшафт)
+            lyrs=r - somehow altered roadmap (измененная карта дорог)
+            lyrs=s - satellite only (вид только со спутника)
+            lyrs=t - terrain only (только ландшафт)
+            lyrs=y - hybrid (гибридная ???)
+
+        Языки:
+            hl=ru - русский язык
+            hl=en - английский язык
+    */
+
+    const GoogleMap = new ol.layer.Tile({
+        title: "GoogleTerrainRoads",
+        source: new ol.source.TileImage({ 
+            url: 'http://mt1.google.com/vt/lyrs=m&hl=ru&x={x}&y={y}&z={z}'
+        }),
+        // visible: false
+        // visible: true
+    })
+
+    // Layer Group - Группа слоёв
+    var layerGroup = new ol.layer.Group({
+        layers: [
+            OSM,
+            GoogleMap
+        ]
+    })
+    map.addLayer(layerGroup)
+
+    // Layer Switcher - Переключатель слоёв
+    const baseLayerElements = document.querySelectorAll('.sidebar-form > label > input[type=radio]')
+    for(let baseLayerElement of baseLayerElements) {
+        baseLayerElement.addEventListener('change', function() {
+            let baseLayerValue = this.value;
+            layerGroup.getLayers().forEach(
+                function(element) {
+                    let baseLayerTitle = element.get('title');
+                    element.setVisible(baseLayerTitle === baseLayerValue);
+            })
+        })
+    }
+
+    // SKFO Data - Северо-Кавказский федеральный округ
+
+    // North Osetia Layer
+    const NorthOsetiaGeoJSON = new ol.layer.VectorImage({
+        source: new ol.source.Vector({
+            url: './data/SKFO/North_Osetia_Republic.geojson',
+            format: new ol.format.GeoJSON()
+        }),
+        visible: true,
+        title: 'SKFO'
+    })
+
+    map.addLayer(NorthOsetiaGeoJSON);
+
+    // Markers
+    var markers = new ol.layer.Vector({
+        source: new ol.source.Vector(),
+        style: new ol.style.Style({
+            image: new ol.style.Icon({
+                // anchor: [1, 1],
+                src: './data/MapMarker.png',
+                scale: 0.05
+            })
+        })
+    });
+    map.addLayer(markers);
+
+    // Get coordinates on map - получаем координаты по клику
+    /*
+    map.on('click', function(e) {
+        console.log("Lon " + e.coordinate[0])
+        console.log("Lat " + e.coordinate[1])
+    })
+    */
+
+    map.on('click', function(e) {
+        var coordinates = Array(1).fill(ol.proj.transform(e.coordinate, 'EPSG:3857', 'EPSG:4326'));
+
+        var marker = new ol.Feature(new ol.geom.Point(ol.proj.fromLonLat([coordinates[0][0], coordinates[0][1]])));
+        markers.getSource().addFeature(marker);
+    });
+}
