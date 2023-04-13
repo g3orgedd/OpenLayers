91437d88fad30ff6c5ad9bc2fe70a6c443c166e6
g3orgedd <g3orgedd@gmail.com>
2023-04-13 12:09:39 +0300

    Added some test files

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import View from 'ol/View.js';
 import { Group as LayerGroup, Tile as TileLayer } from 'ol/layer.js';
 import { fromLonLat } from 'ol/proj.js';
 import { Draw, Modify }from 'ol/interaction';
+import GeoJSON from 'ol/format/GeoJSON.js';
 
 import VectorLayer from 'ol/layer/Vector.js';
 import VectorSource from 'ol/source/Vector.js';
@@ -27,17 +28,20 @@ const map = new Map({
 
 // ============================================================================================
 
-const source1 = new VectorSource();
-const vector1 = new VectorLayer({
-    source: source1,
-});
+// const source1 = new VectorSource();
+// const vector1 = new VectorLayer({
+//     source: source1,
+// });
 
-const source2 = new VectorSource();
-const vector2 = new VectorLayer({
-    source: source2,
-});
+// const source2 = new VectorSource();
+// const vector2 = new VectorLayer({
+//     source: source2,
+// });
 
 // ============================================================================================
+
+const typeSelect = document.getElementById('geom_type');
+// const value = typeSelect.value;
 
 var draw, modify; // global so we can remove it later
 function addInteraction(vector) {
@@ -53,31 +57,113 @@ function addInteraction(vector) {
             source: vector,
         });
         map.addInteraction(modify);
-    }
+    } 
+    typeSelect.onchange = function () {
+        map.removeInteraction(draw);
+        // map.removeInteraction(modify);
+        addInteraction(vector);
+    };
 }
 
 function removeInteraction() {
     map.removeInteraction(draw);
+    // map.removeInteraction(modify);
 }
+// ============================================================================================
+/*
+var selectID = document.getElementById('vector_layers');
 
+var i = 0;
+$(".create1").click(function() {
+    // console.log(markerGroup.getLayers());
+    var vectorLayers = [];
+    var name = 'vectorLayer' + i;
+
+    name = new VectorLayer({
+        source: new VectorSource({
+            url: './geodata/sample/example.geojson',
+            format: new GeoJSON(),
+        }),
+    });
+
+    // markerGroup.getLayers().array_.push(name);
+    // name.setVisible(true);
+    // map.addLayer(name);
+
+    vectorLayers.push(name);
+
+    // selectID.options[selectID.length] = new Option('Layer ' + selectID.length, name);
+    // map.addLayer(name);
+
+    console.log(name);
+    i++;
+});
+*/
 // ============================================================================================
 
-$('.set1').click(function() {
-    removeInteraction(source2);
+// var selectLength = $('#vector_layers > option').length - 1;
+var selectID = document.getElementById('vector_layers');
 
-    map.addLayer(vector1);
-    map.removeLayer(vector2);
-
-    addInteraction(source1);
-});
-
-$('.set2').click(function() {
-    removeInteraction(source1);
+$(".create").click(function() {
+    let newSelectOption = new Option('Слой ' + selectID.length, selectID.length - 1);
+    selectID.options[selectID.length] = newSelectOption;
     
-    map.addLayer(vector2);
-    map.removeLayer(vector1);
-
-    addInteraction(source2);
+    console.log(newSelectOption.value);
 });
 
 // ============================================================================================
+
+var values = new Array();
+
+$("#vector_layers option").each(function() {
+    if($(this).val() !== '') {
+        values.push($(this).val())
+    }
+});
+
+console.log(values);
+
+var sources = new Array();
+var vectors = new Array();
+
+document.getElementById('vector_layers').addEventListener('change', function() {
+    for (let index = 0; index < values.length-1; index++) {
+        var s = new VectorSource();
+        var v = new VectorLayer({
+            source: s
+        })
+    
+        sources.push(s);
+        vectors.push(v);
+    }
+});
+
+// console.log(sources);
+// console.log(vectors);
+
+// ============================================================================================
+
+document.getElementById('vector_layers').addEventListener('change', function() {
+    if (this.value !== 'layer') {
+        for (let index = 0; index < sources.length; index++) {
+            if (index != this.value) {
+                map.removeLayer(vectors[index]);
+                removeInteraction(sources[index]);
+            }
+        }
+
+        addInteraction(sources[this.value]);
+        map.addLayer(vectors[this.value]);
+    }
+
+    // console.log('You selected: ', this.value);
+});
+
+// ============================================================================================
+
+$(document).ready(function() {
+    $('#delete_layer').click(function() {
+        // get the selected option and remove it from the DOM
+        $('#vector_layers option:selected').remove();
+    });
+});
